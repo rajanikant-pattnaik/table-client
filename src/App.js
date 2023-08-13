b1f8c3a56d21f667ad6e024b3e613307c13ce114
unknown <rjpattnaik12345@gmail.com>
2023-08-13 15:54:11 +0530

    another commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ const App = () => {
   const [PhoneNo, setPhoneNo] = useState("");
   const [Email, setEmail] = useState("");
   const [Hobbies, setHobbies] = useState("");
-  const [data, setData] = useState([])
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const [data, setData] = useState([]);
+  const [handle, setHandle] = useState("add");
+  const [_id, set_id] = useState("");
+  const addData = async () => {
     try {
       console.log({ Name, PhoneNo, Email, Hobbies });
       const data = { Name, PhoneNo, Email, Hobbies };
@@ -22,47 +23,181 @@ const App = () => {
       console.log(error);
     }
   };
-  const getAllValue=async()=>{
-    const res=await axios.get('/getAll');
+  const updateData = async () => {
+    try {
+      console.log({ _id, Name, PhoneNo, Email, Hobbies });
+      const data = { _id, Name, PhoneNo, Email, Hobbies };
+      const res = await axios.post("/update", data);
+      console.log(res);
+      setName("");
+      setEmail("");
+      setHobbies("");
+      setPhoneNo("");
+      set_id("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const deleteData = async () => {
+    try {
+      const res = await axios.post("/delete", { _id });
+      console.log(res);
+      setName("");
+      setEmail("");
+      setHobbies("");
+      setPhoneNo("");
+      set_id("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleType = (Name, PhoneNo, Email, Hobbies, type, _id) => {
+    setHandle(type);
+    setName(Name);
+    setEmail(Email);
+    setHobbies(Hobbies);
+    setPhoneNo(PhoneNo);
+    set_id(_id);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (handle === "add") {
+      addData();
+    } else if (handle === "update") {
+      updateData();
+    } else if (handle === "delete") {
+      deleteData();
+    }
+    setHandle("add");
+  };
+  const getAllValue = async () => {
+    const res = await axios.get("/getAll");
     setData(res.data.allData);
-  }
+  };
   useEffect(() => {
     getAllValue();
-  }, [])
-  console.log(data);
+  }, []);
   return (
     <div>
-      <form>
-        <label htmlFor="Name">Name</label>
-        <input
-          type="text"
-          name="Name"
-          value={Name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <label htmlFor="PhoneNo">PhoneNo</label>
-        <input
-          type="text"
-          name="PhoneNo"
-          value={PhoneNo}
-          onChange={(e) => setPhoneNo(e.target.value)}
-        />
-        <label htmlFor="Email">Email</label>
-        <input
-          type="text"
-          name="Email"
-          value={Email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="Hobbies">Hobbies</label>
-        <input
-          type="text"
-          name="Hobbies"
-          value={Hobbies}
-          onChange={(e) => setHobbies(e.target.value)}
-        />
-        <button onClick={handleSubmit}>Submit</button>
+      <form
+        style={{
+          width: "25rem",
+          justifyContent: "center",
+          "margin-left": "20rem",
+        }}
+      >
+        <div className="mb-3">
+          <label htmlFor="Name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            name="Name"
+            value={Name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="PhoneNo" className="form-label">
+            PhoneNo
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            name="PhoneNo"
+            value={PhoneNo}
+            onChange={(e) => setPhoneNo(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Email" className="form-label">
+            Email
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            name="Email"
+            value={Email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Hobbies" className="form-label">
+            Hobbies
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            name="Hobbies"
+            value={Hobbies}
+            onChange={(e) => setHobbies(e.target.value)}
+          />
+        </div>
+        <button onClick={handleSubmit} className="btn btn-primary">
+          {handle}
+        </button>
       </form>
+      <table style={{ marginLeft: "20rem" }}>
+        <tr style={{ marginBottom: "5rem" }}>
+          <th>select</th>
+          <th>serial No</th>
+          <th>Name</th>
+          <th>PhoneNo</th>
+          <th>Email</th>
+          <th>Hobbies</th>
+          <th>Upadate</th>
+          <th>Delete</th>
+        </tr>
+        <tbody>
+          {data.map((currData, index) => {
+            const { _id, Name, PhoneNo, Email, Hobbies } = currData;
+
+            return (
+              <tr key={index} style={{ marginBottom: "5rem" }}>
+                <td>
+                  <input type="checkbox" />
+                </td>
+                <td style={{ textAlign: "center" }}>{index + 1}</td>
+                <td>{Name}</td>
+                <td>{PhoneNo}</td>
+                <td>{Email}</td>
+                <td>{Hobbies}</td>
+                <td>
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleType(Name, PhoneNo, Email, Hobbies, "update", _id);
+                    }}
+                    className="btn btn-primary"
+                  >
+                    update
+                  </button>
+                </td>
+                <td>
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleType(Name, PhoneNo, Email, Hobbies, "delete", _id);
+                    }}
+                    className="btn btn-primary"
+                  >
+                    delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
